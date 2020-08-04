5abef11107e30684432373aeca4748ba65c9af7c
Christian Glaudin <chrismiami05@gmail.com>
2020-08-04 00:06:01 -0400

    Added Solution

---
+++ Bin Merger/Bin Merger/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Bin_Merger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
