9fecd7646cea0cad156fc5254ac870ff3dc89218
Christian Glaudin <chrismiami05@gmail.com>
2020-08-05 01:09:29 -0400

    Added Some Code To Start off

--- Bin Merger/Bin Merger/Program.cs
+++ Bin Merger/Bin Merger/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Diagnostics;
+using System.IO;
 
 namespace Bin_Merger
 {
@@ -7,6 +9,18 @@ namespace Bin_Merger
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            String path = "./PAKPack.exe";
+            Console.WriteLine("Path: " + Path.GetFullPath(path));
+            Process.Start("CMD.exe","/K "+ addQoutes(Path.GetFullPath(path)) );
         }
+
+
+        static string addQoutes(string input)
+        {
+            return "\"" + input + "\"";
+        }
+
+
     }
 }
