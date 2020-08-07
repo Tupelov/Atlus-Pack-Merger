0b4a60516f944bba60809b4451b87d2e85bf50ef
Christian Glaudin <chrismiami05@gmail.com>
2020-08-07 03:52:51 -0400

    Begun Testing On Checksums

--- Bin Merger/Bin Merger/Program.cs
+++ Bin Merger/Bin Merger/Program.cs
@@ -4,6 +4,8 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Net.Http.Headers;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace Bin_Merger
 {
@@ -41,6 +43,25 @@ namespace Bin_Merger
 
             Console.WriteLine("Found: " + modFileAmount);
 
+            Console.WriteLine("Comparing Checksums and Replacing...");
+
+            Console.WriteLine(CalculateMD5(Path.GetFullPath(path)));
+            //In Hind Sight this entire part looks stupid but it works
+            for(int i = 0; i < originalFiles.Count; i++)
+            {
+                for(int k = 0; k< modFiles.Count; k++)
+                {
+                    
+                    string compare = Directory.GetParent(modFiles[k].ToString()).ToString();
+                    if (String.Equals(  Path.GetFileName(compare)  ,Path.GetFileName(originalFiles[i].ToString()) ))
+                    {
+                        Console.WriteLine("IT WORKED");
+                    }
+                }
+            }
+
+
+            //string test = "";
 
 
             //Process.Start("CMD.exe","/K "+ addQoutes(Path.GetFullPath(path)) );
@@ -101,8 +122,24 @@ namespace Bin_Merger
         }
         
         
-        
-        
+        static Array listFilesRecursivly(string directory)
+        {
+
+            string[] filePaths = Directory.GetFiles(directory, "",
+                                         SearchOption.AllDirectories);
+            return filePaths;
+        }
+
+        static string CalculateMD5(string filename)
+        {
+            using var md5 = MD5.Create();
+            using var stream = File.OpenRead(filename);
+            var hash = md5.ComputeHash(stream);
+            return BitConverter.ToString(md5.ComputeHash(stream)).Replace("-", "").ToLower();
+        }
+
+
+
     }
 
     
